d8803052a8b4614a6f78ef4d7e46b2df9c148938
Guillaume Jorandon <guillaume.jorandon@cea.fr>
2017-11-17 14:50:39 +0100

    separate modules

--- backupper/cli.py
+++ backupper/cli.py
@@ -7,69 +7,15 @@ import yaml
 import tarfile
 import datetime
 
-configuration_file = "backupfile.yml"
-backup_datetime = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
-
-def getHelp(command_name):
-    help_string = """Usage: {} [OPTIONS...]
-
-  -h, --help\t\tDisplays the current help and exits.
-  -f, --config-file\tSpecifies an alternative YAML config file (default: {}).
-""".format(command_name, configuration_file)
-
-    return help_string
-
-def validateConfiguration(configuration):
-    # No empty configuration file
-    if not isinstance(configuration, dict):
-        raise Exception("Empty or malformed configuration.")
-
-    # artifacts
-    if not "artifacts" in configuration:
-        raise Exception("Missing \"artifacts\" node.")
-    elif not isinstance(configuration["artifacts"], list):
-        raise Exception("Please provide a list of paths in the \"artifacts\" node.")
-    else:
-        for element in configuration["artifacts"]:
-            if not isinstance(element, str):
-                raise Exception("In \"artifacts\": {} isn't a string.".format(element))
-
-    # delete_old_backups
-    if not "delete_old_backups" in configuration:
-        configuration["delete_old_backups"] = False
-    elif configuration["delete_old_backups"] and not "backup_dir" in configuration:
-        raise Exception("\"delete_old_backups\" is set to true, but there is no \"backup_dir\".")
-    elif not isinstance(configuration["delete_old_backups"], bool):
-        raise Exception("\"delete_old_backups\" should be a boolean.")
-
-    # cleaning_policy
-    valid_cleaning_policies = ["most_recents", "first_daily", "first_weekly", "first_monthly"]
-    if not "cleaning_policy" in configuration or configuration["cleaning_policy"] is None:
-        configuration["cleaning_policy"] = {}
-    elif not isinstance(configuration["cleaning_policy"], dict):
-        raise Exception("\"cleaning_policy\" should be a list of nodes.")
-    else:
-        for key in configuration["cleaning_policy"]:
-            if not key in valid_cleaning_policies:
-                raise Exception("\"{}\" is an incorrect \"cleaning_policy\" option.".format(key))
-            elif not (isinstance(configuration["cleaning_policy"][key], int) and configuration["cleaning_policy"][key] >= 0) :
-                raise Exception("\"{}\" should be a positive integer.".format(key))
-    for policy in valid_cleaning_policies:
-        if not policy in configuration["cleaning_policy"]:
-            configuration["cleaning_policy"][policy] = 0
-
-    # backup_dir
-    if not "backup_dir" in configuration:
-        configuration["backup_dir"] = os.getcwd()
-    if not isinstance(configuration["backup_dir"], str):
-        raise Exception("\"backup_dir\" should be a string.")
+from . import utils
 
 def main():
 
     # Configuration variables
-    global configuration_file
+    configuration_file = "backupfile.yml"
     configuration = None
     command_name = os.path.basename(sys.argv[0])
+    backup_datetime = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
 
     ## Initialisation ##
 
@@ -83,7 +29,7 @@ def main():
 
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            sys.stdout.write(getHelp(command_name))
+            sys.stdout.write(utils.getHelp(command_name, configuration_file))
             sys.exit(0)
         if opt in ("-f", "--config-file"):
             configuration_file = str(arg)
@@ -99,7 +45,7 @@ def main():
 
     # Validate the configuration file
     try:
-        validateConfiguration(configuration)
+        utils.validateConfiguration(configuration)
     except Exception as e:
         sys.stderr.write("Error: configuration validation: {}\n".format(e))
         sys.exit(3)
